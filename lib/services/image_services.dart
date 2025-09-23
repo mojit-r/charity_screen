66c0cd54bea90e7c12d9d2eed17dbf8d1b95bf58
mojit-r <rajpurohitmohit0323@gmail.com>
2025-09-23 13:38:28 +0530

    changed perPage and lowered the quality of the image

--- lib/services/image_services.dart
+++ lib/services/image_services.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 class ImageServices {
   final String accessKey = dotenv.env['UNSPLASH_KEY'] ?? '';
 
-  Future<List<String>> fetchImages(String query, {int perPage = 5}) async {
+  Future<List<String>> fetchImages(String query, {int perPage = 30}) async {
     final url = Uri.parse(
       'https://api.unsplash.com/search/photos?query=$query&per_page=$perPage',
     );
@@ -18,7 +18,7 @@ class ImageServices {
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
       List results = data['results'];
-      return results.map<String>((img) => img['urls']['regular']).toList();
+      return results.map<String>((img) => img['urls']['small']).toList();
     } else {
       throw Exception('Failed to load images');
     }
